35ce563a7991a065db75e04d8850d7186f4bf9e2
serp-master <83224714+serp-master@users.noreply.github.com>
2021-04-26 19:13:25 +0300

    Create python_news_api.py

---
+++ Google News API/python_news_api.py
@@ -0,0 +1,29 @@
+import requests
+from pprint import pprint
+
+# Specify content type and form the request body
+headers = {'Content-Type': 'application/json'}
+
+job_params = {
+    'scraper': 'google_search',
+    'tbm': 'nws',
+    'q': 'restaurants near me',
+    'domain': 'com',
+    'parse': 'true',
+    'device': 'mobile',
+    'geo': 'Boston,Massachusetts,United States',
+    'locale': 'en-us',
+    'page': '1'
+}
+
+# Post job and get response
+response = requests.request(
+    'POST',
+    'https://rt.serpmaster.com/',
+    headers=headers,
+    json=job_params,
+    auth=('username', 'password')
+)
+
+#Print the response body
+pprint(response.json())
