8bfe726410908010cf1a9fdf650b39ef322449aa
serp-master <83224714+serp-master@users.noreply.github.com>
2021-04-28 09:55:14 +0300

    Create node_image_api.js

---
+++ Google Image API/node_image_api.js
@@ -0,0 +1,20 @@
+const axios = require('axios')
+
+const params = {
+  'scraper': 'google_search',
+  'tbm': 'isch',
+  'q': 'restaurants near me',
+  'domain': 'com',
+  'parse': 'true',
+  'device': 'mobile',
+  'geo': 'Boston,Massachusetts,United States',
+  'locale': 'en-us',
+  'page': '1',
+  'access_token': 'ACCESS_TOKEN' //Your access token from SERPMaster
+}
+
+axios.get('https://rt.serpmaster.com/', {
+  params: params
+})
+  .then(response => console.log(response))
+  .catch(error => console.log(error))
